a18b15729beb3316d93b3c9b1ce9da8e4764ae70
jonyglz <gon18025@byui.edu>
2021-06-03 22:04:11 -0600

    Lazy loader

---
+++ lesson7/js/lazy.js
@@ -0,0 +1,17 @@
+const imagesToLoad= document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    treshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload=()=>{image.removeAttribute("data-src");};
+};
+
+if("IntersectionObserver" in window){
+    const imgObserver=new IntersectionObserver((items,observer)=>{
+        items.forEach((item)=>{});
+    })
+}
